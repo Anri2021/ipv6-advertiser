1551818f3ee741acfdb2131319e1d8c94c5445c9
Anri2021 <anri.il.2019@gmail.com>
2026-09-14 00:40:50 +0300

    Add additional config argument options

--- ipv6-advertiser.py
+++ ipv6-advertiser.py
@@ -57,7 +57,7 @@ PRESENCE_COOLDOWN = 30.0
 # טעינת קובץ תצורה אופציונלי בעל אותו שם של הסקריפט (.conf או .json)
 def _resolve_config_path() -> Path:
     for idx, arg in enumerate(sys.argv[:-1]):
-        if arg.lower() in ("-c", "--config", "-config", "-configfile", "--configfile"):
+        if arg.lower() in ("-c", "--conf", "-conf","--config", "-config", "-configfile", "--configfile"):
             return Path(sys.argv[idx + 1]).resolve()
     env_path = os.getenv("RA_CONFIG_PATH")
     if env_path:
